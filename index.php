fb95040539a30f5bafa8f251865306367bbcd741
rlcayaco <43124226+rlcayaco@users.noreply.github.com>
2019-01-25 20:34:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+        include_once 'pme-header.php';
+    ?>
+</head>
+
+<body>
+<!-- HEAD -->
+    <?php
+        include_once 'pme-navbar.php';
+    ?>
+
+
+<!-- BODY -->       
+<div class="container-fluid outer-body">
+    <div class="inner-body">
+
+        <!-- upper-box -->
+        <div class="container-fluid index-upper-box">
+
+            <!-- index-animation -->
+            <div class="card bg-dark text-white">
+                <!-- image -->
+                <div class="index-main-image">
+                    <img src="index-background-pics/bkgdHomePME.png" alt="bkgdHomePME">
+                </div>
+
+                <!-- overlay -->
+                <div class="card-img-overlay">
+                    <div class="cio2">
+                        
+                        <div class="row">
+
+                            <div class="col-12 col-lg-12 cio2-row-inner-top">
+                                <div class="owl-wrapper1">
+                                    <div class="owl-carousel owlcarousel1">
+                                        <!-- MIDAS -->
+                                        <div class="item "><img class="carousel1-logo" src="software-logos/midas/midasIT-logo.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/gen_main.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/gen2.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/gen3.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/midasGen2.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/midasGen3.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/midasNFX1.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/midas/midasNFX2.png"></div>
+                                        <!-- AUTODESK -->
+                                        <div class="item "><img class="carousel1-logo" src="software-logos/autocad/autodesk-logo2.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/autocad/Architecture1.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/autocad/Autocad-Electrical.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/autocad/mep1.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/autocad/mep2.png"></div>
+                        
+                                        <!-- INSIGHT -->
+                                        <div class="item "><img class="carousel1-software" src="software-logos/insight/autodesk-insight-logo.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/insight/autodesk-insight.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/insight/insight2.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/insight/insight3.jpg"></div>
+                                        <!-- SKETCH-UP -->
+                                        <div class="item "><img class="carousel1-logo" src="software-logos/sketchup/software-logo2.jpg"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/sketchup/sketchup1.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/sketchup/sketchup2.png"></div>
+                                        <div class="item "><img class="carousel1-software" src="software-logos/sketchup/sketchup3.png"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-lg-12 cio2-row-inner-bottom">
+                                <div class="cio2-col-bottom-inner">
+                                    <p class="cio2-title1">Project Management Engineering Technic Inc.</p>
+                                    <p class="cio2-title2">End to End Project Solutions Provider</p>
+                                    <div class="cio2-btn"><a href="pme-contact.php" class="btn btn-info" role="button">CONTACT US NOW! <i class="fas fa-phone-square"></i></a></div>
+                                </div>
+                            </div>
+
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+
+            <!-- Index News -->
+            <div class="index-news-container">
+                
+                <div class="row owlcarousel3-container">
+
+                    <div class="col-1 col-md-2 owlcarousel3-col-outer">
+                        <div class="owlcarousel3-newstitle-container">
+                            <p class="owlcarousel3-newstitle">Latest News:</p>
+                        </div>
+                    </div>
+                    
+                    <div class="col-11 col-md-10 owlcarousel3-col-outer">
+                        <div class="owl-carousel owlcarousel3">
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-4 col-sm-3 col-md-2 owlcarousel3-col-inner"><img class="carousel3-image" src="buildings/proj-2017/pme-technic.jpg"></div>
+                                    <div class="col-8 col-sm-9 col-md-10 owlcarousel3-col-inner"><p class="carousel3-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eligendi sequi</p></div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-4 col-sm-3 col-md-2 owlcarousel3-col-inner"><img class="carousel3-image" src="buildings\proj-2018\2018-mondelez.jpg"></div>
+                                    <div class="col-8 col-sm-9 col-md-10 owlcarousel3-col-inner"><p class="carousel3-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eligendi sequi</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
+        </div>
+
+        <!-- lower-box -->
+        <div class="lower-box">
+            
+            <!-- ABOUT US -->
+            <div class="world-container">
+                <div class="world-padding">
+                    <p class="index-title-main">ABOUT US</p>
+                    
+                    <div class="world-row-center aboutus-row-container">
+                        <div class="row world-row2 aboutus-row-inner">
+                            <div class="col-12 col-md-7 index-col-left">
+                                <div class="card background-black">
+                                    <img class="card-img whoweare-image" src="index-background-pics/pexels-photo-371900.jpeg" alt="Card image">
+                                    <div class="card-img-overlay">
+                                        <div class="large-section">
+                                        <p class="index-aboutus-description"><span class="w-title2">PMENG'G TECHNIC INC.</span> (PME Technic) is a 100% local company which was conceived to offer 
+                                            various specialized projects engineering services such as design conceptualization, detail 
+                                            engineering, bid documentations, package tendering, project/construction management, 
+                                            consultancy works, value engineering, green building and renewable energy objectives, 
+                                            supply of project components, compliance to manufacturing system’s operational requirements, 
+                                            safety and environmental engineering.
+                                        </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-5 index-col-right"><img class="whoweare-image" src="index-background-pics/aboutUsPicture1.jpg" alt=""></div>
+                        </div>
+                    </div>
+
+                    <div class="world-row-center aboutus-row-container">
+                        <div class="row world-row2 aboutus-row-inner">
+                            <div class="col-12 col-md-5 index-col-left"><img class="whoweare-image" src="index-background-pics/aboutUsPicture2.jpg" alt=""></div>
+                            <div class="col-12 col-md-7 index-col-right"><img class="whoweare-image" src="index-background-pics/aboutUsPicture3.jpg" alt=""></div>
+                        </div>
+                    </div>
+
+                    <!-- index-link -->
+                    <div class="row justify-content-end index-link-container">
+                        <div class="col-auto">
+                            <a href="pme-aboutUs.php"><i class="fas fa-arrow-alt-circle-right"></i> MORE OF US</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- lower-box WHATWEDO  -->
+            <div class="index-whatwedo-container">
+                <div class="index-whatwedo-container-inner">
+                    <p class="index-title-main">DELIVERABLES</p>
+                    <div class="row wwd-row">
+
+                        <div class="col-sm lb-index-left">
+                            
+                            <div class="lb-index-left-inner">
+                                <div class="lb-index-title">
+            
+                                    <ul>
+                                        <li class="lb-title-big">Project Management</li>
+                                        <li class="lb-title-big">Engineering</li>
+                                    </ul>
+                            
+                                </div>
+                                <div class="lb-index-image">
+                                    <img src="index-background-pics/deliverablesPic1.jpg" alt="">
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="col-sm lb-index-right">
+                            <div class="lb-index-list">
+                                <ul>
+                                    <li><p class="lb-index-list-title">Initial Engineering<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Detail Engineering<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">PM/CM Works<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Due Diligence/Risk Management Studies<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Construction Works Package Tendering<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Other Engineering Works<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Design & Build Packages<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                    <li><p class="lb-index-list-title">Renewable Energy & Green Bldg. Technology<span class="float-right"><i class="fas fa-angle-double-right"></i></span></p></li>
+                                </ul>
+                            </div>
+                        </div>
+                    <!-- end of row -->
+                    </div>
+
+                    <!-- index-link -->
+                    <div class="row justify-content-end index-link-container">
+                        <div class="col-auto">
+                            <a href="pme-aboutUs.php"><i class="fas fa-arrow-alt-circle-right"></i> DELIVERABLES</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            <!-- end of whatwedo container -->
+            </div>
+
+            <!-- NEWS -->
+            <div class="world-container">
+                <div class="world-padding">
+                    <p class="index-title-main">LATEST NEWS</p>
+                    <div class="world-row-center prj-border-bottom">
+                        <div class="row world-row2">
+                            <div class="col-sm-4 index-news-col-left"><img class="news-image img-fluid" src="index-background-pics/aboutUsPicture2.jpg" alt=""></div>
+                            <div class="col-sm-8 index-news-col-right">
+                                <p class="world-description">(PME Technic) is a 100% local company which was conceived to offer 
+                                    various specialized projects engineering services such as design conceptualization, detail 
+                                    engineering, bid documentations, package tendering, project / construction management, 
+                                    consultancy works, value engineering, green building and renewable energy objectives, 
+                                    supply of project components, compliance to manufacturing system’s operational requirements, 
+                                    safety and environmental engineering.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="world-row-center prj-border-bottom">
+                        <div class="row world-row2">
+                            <div class="col-sm-4 index-news-col-left"><img class="news-image img-fluid" src="index-background-pics/aboutUsPicture2.jpg" alt=""></div>
+                            <div class="col-sm-8 index-news-col-right">
+                                <p class="world-description">(PME Technic) is a 100% local company which was conceived to offer 
+                                    various specialized projects engineering services such as design conceptualization, detail 
+                                    engineering, bid documentations, package tendering, project / construction management, 
+                                    consultancy works, value engineering, green building and renewable energy objectives, 
+                                    supply of project components, compliance to manufacturing system’s operational requirements, 
+                                    safety and environmental engineering.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- index-link -->
+                    <div class="row justify-content-end index-link-container">
+                        <div class="col-auto">
+                            <a href="#"><i class="fas fa-arrow-alt-circle-right"></i> MORE NEWS</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- PROJECTS -->
+            <div class="projects-container">
+                <div class="projects-container-inner">
+                    <p class="index-title-main" >PROJECTS</p>
+                    <div class="projects-card-deck-container">
+                        <div class="card-deck">
+                            <div class="card projects-card">
+                                <img class="card-img-top" src="buildings/proj-2017/pme-technic2.jpg" alt="Card image cap">
+                                <div class="card-body projects-body">
+                                    <div>
+                                    <p class="card-text projects-description">2018</p>
+                                    <p class="card-test projects-title">PMENGG TECHNIC INC.</p>
+                                    <p class="card-text projects-description">(Sta. Ana, Manila)</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card projects-card">
+                                <img class="card-img-top" src="buildings\proj-2018\2018-mondelez.jpg" alt="Card image cap">
+                                <div class="card-body projects-body">
+                                    <div>
+                                    <p class="card-text projects-description">2018</p>
+                                    <p class="card-test projects-title">MONDELEZ PHILIPPINES INC.</p>
+                                    <p class="card-text projects-description">(Parañaque, Metro Manila)</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card projects-card">
+                                <img class="card-img-top" src="buildings\proj-2017\2017-farmesa.jpg" alt="Card image cap">
+                                <div class="card-body projects-body">
+                                    <div>
+                                    <p class="card-text projects-description">2017</p>
+                                    <p class="card-test projects-title">FARMESA ASIA PACIFIC INC. (FAPI)</p>
+                                    <p class="card-text projects-description">(LISP4 Malvar, Batangas)</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- index-link -->
+                    <div class="row justify-content-end index-link-container">
+                        <div class="col-auto">
+                            <a href="pme-projects-main.php"><i class="fas fa-arrow-alt-circle-right"></i> MORE PROJECTS</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            
+            
+            
+        <!-- end of lower-box -->
+        </div>
+
+    </div>
+</div>   
+
+
+
+<!-- FOOTER --> 
+    <?php
+        include_once 'pme-footer.php';
+    ?>
+    <script src="script-index.js?<?php echo time(); ?>"></script>
+</body>
+
+</html>
+
+
